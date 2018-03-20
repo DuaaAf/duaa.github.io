8f9cf39ccb98d727f50a59edd9bd085ee07716b8
DuaaAf <37474432+DuaaAf@users.noreply.github.com>
2018-03-20 19:38:09 +0200

    Add files via upload

---
+++ yes.js
@@ -0,0 +1,10 @@
+/*var btn= document.getElementById("btn"),
+myAttr= document.createAttribute("audio");
+myAttr.src="file:///D:/songs/Lady_Ga_ga_-_Alejandro.mp3";
+btn.onclick=function(){
+    'use strict';
+    btn.setAttributeNode(myAttr);
+};*/
+location.host="duaa.github.io";
+    
+
